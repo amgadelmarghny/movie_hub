b985cf3d4b5bed56dd136968d44c7cadc59ffa7f
Amgad Elmarghny <amgadsaad22@gmail.com>
2024-12-31 13:31:59 +0200

    create movie model

---
+++ lib/features/Movies/data/models/movie_model.dart
@@ -0,0 +1,25 @@
+import 'package:movie_hub/features/Movies/domain/entities/movie.dart';
+
+class MovieModel extends Movie {
+  const MovieModel({
+    required super.id,
+    required super.title,
+    required super.backdropPath,
+    required super.overview,
+    required super.voteAverage,
+    required super.genreIds,
+    required super.releaseDate,
+  });
+
+  factory MovieModel.fromJson(Map<String, dynamic> json) {
+    return MovieModel(
+      id: json['id'],
+      title: json['title'],
+      backdropPath: json['backdrop_path'],
+      overview: json['overview'],
+      voteAverage: json['vote_average'],
+      genreIds: List<int>.from(json['genre_ids'].map((e) => e)),
+      releaseDate: json['release_date'],
+    );
+  }
+}
