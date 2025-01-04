cbb39b672519706aafb6623e1abef8b9185a2c46
Amgad Elmarghny <amgadsaad22@gmail.com>
2025-01-04 13:46:18 +0200

    movie repo refactore

--- lib/features/Movies/data/repositories/movie_repo.dart
+++ lib/features/Movies/data/repositories/movie_repo.dart
@@ -1,6 +1,7 @@
 import 'package:movie_hub/core/error/failure.dart';
 import 'package:movie_hub/core/network/server_exception.dart';
 import 'package:movie_hub/features/Movies/data/datasources/remote/movie_remote_data_source.dart';
+import 'package:movie_hub/features/Movies/data/models/movie_model.dart';
 import 'package:movie_hub/features/Movies/domain/entities/movie.dart';
 import 'package:movie_hub/features/Movies/domain/repositories/base_movie_repo.dart';
 import 'package:dartz/dartz.dart';
@@ -13,26 +14,22 @@ class MovieRepo implements BaseMoviesRepo {
   @override
   Future<Either<Failure, List<Movie>>> getNowPlayingMovies() async {
     final result = await baseMovieDataSource.getNowPlayingMovies();
-    try {
-      return Right(result);
-    } on ServerException catch (exception) {
-      return Left(ServerFailure(message: exception.errorModel.message));
-    }
+    return returnOperationMethod(result);
   }
 
   @override
   Future<Either<Failure, List<Movie>>> getPopularMovies() async {
     final result = await baseMovieDataSource.getPopularMovies();
-    try {
-      return Right(result);
-    } on ServerException catch (exception) {
-      return Left(ServerFailure(message: exception.errorModel.message));
-    }
+    return returnOperationMethod(result);
   }
 
   @override
   Future<Either<Failure, List<Movie>>> getTopRatedMovies() async {
     final result = await baseMovieDataSource.getTopRatedMovies();
+    return returnOperationMethod(result);
+  }
+
+  Either<Failure, List<Movie>> returnOperationMethod(List<MovieModel> result) {
     try {
       return Right(result);
     } on ServerException catch (exception) {
